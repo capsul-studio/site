6371c1d663283747547fb98438b7c3c97b2fbd15
Gui Sarmento <gui@memberful.com>
2022-01-24 14:53:18 -0300

    fix functions timezone

--- netlify/functions/helpers/shared.js
+++ netlify/functions/helpers/shared.js
@@ -17,11 +17,11 @@ function omit (obj, keys) {
 }
 
 const toReadableHour = (dateString) => {
-  return new Intl.DateTimeFormat('en-CA', { hour: 'numeric', minute: 'numeric', hour12: false }).format(new Date(dateString))
+  return new Intl.DateTimeFormat('en-CA', { hour: 'numeric', minute: 'numeric', hour12: false, timezone: 'America/Toronto' }).format(new Date(dateString))
 }
 
 const toReadableDate = (dateString) => {
-  return new Intl.DateTimeFormat('en-CA', { day: 'numeric', month: 'long', year: 'numeric' }).format(new Date(dateString))
+  return new Intl.DateTimeFormat('en-CA', { day: 'numeric', month: 'long', year: 'numeric', timezone: 'America/Toronto' }).format(new Date(dateString))
 }
 
 module.exports = {
